7774f5d59809415d736f3c2628b02b83b931c0c2
Sawan Vithlani <sawan@kili-soft.com>
2015-08-02 17:04:37 +1000

    Upgrade packages

---
+++ elpa/fuzzy-20150729.2037/fuzzy-autoloads.el
@@ -0,0 +1,15 @@
+;;; fuzzy-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("fuzzy.el") (21947 26302 988870 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; fuzzy-autoloads.el ends here
