108c3ce6bd901ac3a887084d8178aef8aa42374f
Sawan Vithlani <sawan@kili-soft.com>
2016-04-07 02:05:54 +0100

    Upgrade packages

--- elpa/suscolors-theme-20160311.458/suscolors-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "suscolors-theme" "20160311.458" "Colorful theme, inspired by Gruvbox." 'nil :stars '("") :url "https://github.com/TheSuspiciousWombat/SusColors-emacs")
---
+++ elpa/suscolors-theme-20160319.1743/suscolors-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "suscolors-theme" "20160319.1743" "Colorful theme, inspired by Gruvbox." 'nil :stars '("") :url "https://github.com/TheSuspiciousWombat/SusColors-emacs")
