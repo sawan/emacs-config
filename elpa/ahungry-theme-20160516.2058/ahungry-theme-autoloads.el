19ba9b9875471aa6e1ae836f679ce6fc18098ec8
Sawan Vithlani <sawan@kili-soft.com>
2016-05-27 21:08:00 +1000

    Revert "Upgrade packages"
    This reverts commit 719dffd46e77cb9c263e3a36de5ca271252e3d3a.

--- elpa/ahungry-theme-20160516.2058/ahungry-theme-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; ahungry-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-<<<<<<< HEAD:elpa/ahungry-theme-20160516.2058/ahungry-theme-autoloads.el
-;;;### (autoloads nil "ahungry-theme" "ahungry-theme.el" (22342 63111
-=======
-;;;### (autoloads nil "ahungry-theme" "ahungry-theme.el" (22334 45470
->>>>>>> d946b38dc8004fffa247ff073cbe98951b4bc76e:elpa/ahungry-theme-20160516.2058/ahungry-theme-autoloads.el
-;;;;;;  0 0))
-;;; Generated autoloads from ahungry-theme.el
-
-(when (and load-file-name (boundp 'custom-theme-load-path)) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ahungry-theme-autoloads.el ends here
--- elpa/avy-20160519.536/avy-pkg.el
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-(define-package "avy" "20160519.536" "tree-based completion" '((emacs "24.1") (cl-lib "0.5")) :stars '(#("360" 0 3 (font-lock-face paradox-star-face))) :url "https://github.com/abo-abo/avy" :keywords '("point" "location"))
-=======
-(define-package "avy" "20160519.536" "tree-based completion" '((emacs "24.1") (cl-lib "0.5")) :stars '("") :url "https://github.com/abo-abo/avy" :keywords '("point" "location"))
->>>>>>> d946b38dc8004fffa247ff073cbe98951b4bc76e
