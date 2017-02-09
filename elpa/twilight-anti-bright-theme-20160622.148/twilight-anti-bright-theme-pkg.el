4bcae0dd58d224f8043e16c4abc04d60fcbb54ec
Sawan Vithlani <sawan@kili-soft.com>
2017-02-09 21:16:30 +1100

    Themes

---
+++ elpa/twilight-anti-bright-theme-20160622.148/twilight-anti-bright-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "twilight-anti-bright-theme" "20160622.148" "A soothing Emacs 24 light-on-dark theme" 'nil :stars '("") :url "https://github.com/jimeh/twilight-anti-bright-theme.el" :keywords '("themes"))
