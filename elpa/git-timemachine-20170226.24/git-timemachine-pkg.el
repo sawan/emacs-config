1c31405157d6bb2b3fc990a7d5fd7a2c780eb9d3
Sawan Vithlani <sawan@kili-soft.com>
2017-03-05 11:30:33 +1100

    Updates

---
+++ elpa/git-timemachine-20170226.24/git-timemachine-pkg.el
@@ -0,0 +1,1 @@
+(define-package "git-timemachine" "20170226.24" "Walk through git revisions of a file" '((emacs "24.3")) :stars '("") :url "https://github.com/pidu/git-timemachine" :keywords '("git"))
