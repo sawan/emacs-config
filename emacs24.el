74ea4bdf07bd9a5d4c45d37d1313edb03d5d9c7c
Sawan Vithlani <sawan@kili-soft.com>
2017-03-22 12:41:37 +1100

    Remove pretty-mode from config, tidy up Python coding helper functions

--- emacs24.el
+++ emacs24.el
@@ -282,13 +282,6 @@
 (setq swoop-use-target-magnifier-around: 10)
 (setq swoop-use-target-magnifier-size: 1.2)
 
-;; (require 'pretty-mode)
-;; if you want to set it globally
-;; (global-pretty-mode t)
-
-;; if you want to set it only for a specific mode
-;;(add-hook 'my-pretty-language-hook 'turn-on-pretty-mode)
-
 ;; Company mode
 (eval-after-load 'company
   '(progn
@@ -971,14 +964,15 @@ Version 2015-02-07
 
 (defhydra hydra-bookmarks ()
   "Bookmarks"
-  ("s" bm-toggle "toggle" :color red)
+  ("t" bm-toggle "toggle" :color red)
   ("n" bm-next   "next"   :color red)
   ("p" bm-previous "previous" :color red)
-  ("a" bm-show "show" :color blue)
-  ("A" bm-show-all "SHOW" :color blue)
+  ("s" bm-show "show" :color blue)
+  ("S" bm-show-all "SHOW" :color blue)
   ("c" bm-remove-all-current-buffer "clear" :color blue)
   ("l" bm-bookmark-line "line" :color blue)
   ("r" bm-bookmark-regexp "regex" :color blue)
+  ("w" bm-save "save" :color blue)
   ("q" nil :color red)
   )
 
@@ -1058,8 +1052,10 @@ Version 2015-02-07
                                             ("Spray-chars" . spray-chars))))))))
 (add-hook 'artist-mode-init-hook
           (lambda ()
-            (define-key artist-mode-map (kbd "C-c C-a C-o") 'artist-ido-select-operation)
-            (define-key artist-mode-map (kbd "C-c C-a C-c") 'artist-ido-select-settings)))
+            (define-key artist-mode-map
+	      (kbd "C-c C-a C-o") 'artist-ido-select-operation)
+            (define-key artist-mode-map
+	      (kbd "C-c C-a C-c") 'artist-ido-select-settings)))
 
 
 ;;;; smex
@@ -1155,11 +1151,6 @@ Version 2015-02-07
 ;;;; python mode
 (require 'python)
 
-;; Rebind RET
-;; (add-hook 'python-mode-hook '(lambda ()
-			       ;; (define-key python-mode-map
-				 ;; (kbd "RET") 'newline-and-indent)))
-
 (defun python-add-debug-highlight ()
   "Adds a highlighter for use by `python-pdb-breakpoint-string'"
   (highlight-lines-matching-regexp "## DEBUG ##\\s-*$" 'hi-red-b))
@@ -1168,18 +1159,31 @@ Version 2015-02-07
 
 (defvar python-pdb-breakpoint-string
   ;;"from pudb import set_trace;set_trace() ## DEBUG ##"
-  "import ipdb,pprint;pp=pprint.PrettyPrinter(width=2,indent=2).pprint;ipdb.set_trace() ## DEBUG ##"
+  "import ipdb,pprint;\
+pp=pprint.PrettyPrinter(width=2,indent=2).pprint;\
+ipdb.set_trace(); ## DEBUG ##"
   "Python breakpoint string used by `python-insert-breakpoint'")
 
 (defun python-insert-breakpoint ()
   "Inserts a python breakpoint using `ipdb'"
   (interactive)
-  (back-to-indentation)
   ;; this preserves the correct indentation in case the line above
   ;; point is a nested block
   (insert python-pdb-breakpoint-string)
   (back-to-indentation)
 )
+
+(defun python-remove-debug-breaks ()
+   "Removes all debug breakpoints"
+   (flush-lines "\#\# DEBUG \#\#"))
+
+(defun pdb ()
+  (interactive)
+  (python-insert-breakpoint))
+
+(defun rpdb()
+  (interactive)
+  (python-remove-debug-breaks))
 
 (defun python-insert-string(in-string)
   "Inserts string"
@@ -1190,9 +1194,6 @@ Version 2015-02-07
   (python-indent-line)
   (backward-char 3))
 
-(defun python-remove-debug-breaks ()
-   "Removes all debug breakpoints"
-   (flush-lines "## DEBUG ##\\s-*$"))
 
 (add-hook 'python-mode-hook 'python-remove-debug-breaks)
 
@@ -1492,13 +1493,13 @@ Other buffers: %s(my/number-names my/last-buffers) I: ibuffer q: quit w: other-w
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+ ;; If          there is more than one, they won't work right.
  '(elpy-modules
    (quote
     (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults)))
  '(package-selected-packages
    (quote
-    (color-theme-actress color-theme-approximate color-theme-cobalt color-theme-complexity color-theme-dg color-theme-dpaste color-theme-eclipse color-theme-emacs-revert-theme color-theme-github color-theme-gruber-darker color-theme-heroku color-theme-ir-black color-theme-library color-theme-modern color-theme-molokai color-theme-monokai color-theme-railscasts color-theme-sanityinc-solarized color-theme-sanityinc-tomorrow color-theme-tango color-theme-tangotango color-theme-twilight color-theme-vim-insert-mode color-theme-wombat color-theme-x color-theme-zenburn colour-region color-theme-buffer-local company-dict company-emoji company-shell zerodark-theme zenburn-theme zen-and-art-theme yaml-mode wttrin wrap-region wide-n volatile-highlights visual-regexp-steroids visible-mark virtualenv undo-tree twilight-theme twilight-bright-theme twilight-anti-bright-theme tommyh-theme tj-mode tangotango-theme syntax-subword swoop swiper suscolors-theme soothe-theme solarized-theme soft-morning-theme smyx-theme smooth-scrolling smooth-scroll smex smart-mode-line-powerline-theme react-snippets rainbow-mode rainbow-delimiters pretty-mode pos-tip plur paredit paradox ov origami nose noctilux-theme nginx-mode names multiple-cursors move-text moe-theme markdown-mode+ magit-push-remote macrostep macros+ leuven-theme key-chord jsx-mode jedi jazz-theme itail iregister iedit idomenu ido-ubiquitous hungry-delete hemisu-theme hc-zenburn-theme guide-key gruber-darker-theme grandshell-theme google-this git-timemachine fuzzy full-ack firecode-theme firebelly-theme fastnav faff-theme expand-region espresso-theme emmet-mode elpy ecb easy-kill-extras doom-themes django-theme django-snippets django-mode django-manage distinguished-theme display-theme deft darkmine-theme darkburn-theme darkane-theme dark-mint-theme danneskjold-theme cyberpunk-theme csv-mode color-theme-solarized color-moccur cherry-blossom-theme bug-hunter bubbleberry-theme browse-kill-ring boxquote bm bliss-theme birds-of-paradise-plus-theme beacon basic-theme badger-theme back-button autumn-light-theme autopair aurora-theme atom-one-dark-theme atom-dark-theme angry-police-captain ample-zen-theme ample-theme ample-regexps ahungry-theme aggressive-indent ag ace-window ace-link ace-jump-zap ace-jump-buffer ace-isearch)))
+    (color-theme-actress color-theme-approximate color-theme-cobalt color-theme-complexity color-theme-dg color-theme-dpaste color-theme-eclipse color-theme-emacs-revert-theme color-theme-github color-theme-gruber-darker color-theme-heroku color-theme-ir-black color-theme-library color-theme-modern color-theme-molokai color-theme-monokai color-theme-railscasts color-theme-sanityinc-solarized color-theme-sanityinc-tomorrow color-theme-tango color-theme-tangotango color-theme-twilight color-theme-vim-insert-mode color-theme-wombat color-theme-x color-theme-zenburn colour-region color-theme-buffer-local company-dict company-emoji company-shell zerodark-theme zenburn-theme zen-and-art-theme yaml-mode wttrin wrap-region wide-n volatile-highlights visual-regexp-steroids visible-mark virtualenv undo-tree twilight-theme twilight-bright-theme twilight-anti-bright-theme tommyh-theme tj-mode tangotango-theme syntax-subword swoop swiper suscolors-theme soothe-theme solarized-theme soft-morning-theme smyx-theme smooth-scrolling smooth-scroll smex smart-mode-line-powerline-theme react-snippets rainbow-mode rainbow-delimiters pos-tip plur paredit paradox ov origami nose noctilux-theme nginx-mode names multiple-cursors move-text moe-theme markdown-mode+ magit-push-remote macrostep macros+ leuven-theme key-chord jsx-mode jedi jazz-theme itail iregister iedit idomenu ido-ubiquitous hungry-delete hemisu-theme hc-zenburn-theme guide-key gruber-darker-theme grandshell-theme google-this git-timemachine fuzzy full-ack firecode-theme firebelly-theme fastnav faff-theme expand-region espresso-theme emmet-mode elpy ecb easy-kill-extras doom-themes django-theme django-snippets django-mode django-manage distinguished-theme display-theme deft darkmine-theme darkburn-theme darkane-theme dark-mint-theme danneskjold-theme cyberpunk-theme csv-mode color-theme-solarized color-moccur cherry-blossom-theme bug-hunter bubbleberry-theme browse-kill-ring boxquote bm bliss-theme birds-of-paradise-plus-theme beacon basic-theme badger-theme back-button autumn-light-theme autopair aurora-theme atom-one-dark-theme atom-dark-theme angry-police-captain ample-zen-theme ample-theme ample-regexps ahungry-theme aggressive-indent ag ace-window ace-link ace-jump-zap ace-jump-buffer ace-isearch)))
  '(paradox-github-token t))
 
 (custom-set-faces
