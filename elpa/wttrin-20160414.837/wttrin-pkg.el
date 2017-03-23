fc350f7b8c9014b0c4ec8afe22ed412fcff8270f
Sawan Vithlani <sawan@kili-soft.com>
2017-03-23 21:53:24 +1100

    Removed package

--- elpa/wttrin-20160414.837/wttrin-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "wttrin" "20160414.837" "Emacs frontend for weather web service wttr.in" '((emacs "24.4") (xterm-color "1.0")) :stars '("") :url "https://github.com/bcbcarl/emacs-wttrin" :keywords '("comm" "weather" "wttrin"))
