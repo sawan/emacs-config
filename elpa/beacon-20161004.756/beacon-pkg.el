461eb62af1ec9d9923dd47084c351c44dd8538ef
Sawan Vithlani <sawan@kili-soft.com>
2017-04-05 20:33:13 +1000

    Packages

--- elpa/beacon-20161004.756/beacon-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "beacon" "20161004.756" "Highlight the cursor whenever the window scrolls" '((seq "2.14")) :stars '(#("128" 0 3 (font-lock-face paradox-star-face))) :url "https://github.com/Malabarba/beacon" :keywords '("convenience"))
