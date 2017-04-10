9c74f63df7303d719686ad17b885416c3dcc0086
Sawan Vithlani <sawan@kili-soft.com>
2017-04-10 23:37:25 +1000

    Upgrade packages

--- elpa/nord-theme-20170325.2314/nord-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "nord-theme" "20170325.2314" "An arctic, north-bluish clean and elegant theme" '((emacs "24")) :stars '("") :url "https://github.com/arcticicestudio/nord-emacs")
