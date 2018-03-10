de146dffe742829f118006702fb0700cd211d8a4
Sawan Vithlani <sawan@kili-soft.com>
2018-03-10 13:25:42 +1100

    Package updates

---
+++ elpa/ace-link-20180308.100/ace-link-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ace-link" "20180308.100" "Quickly follow links" '((avy "0.4.0")) :stars '(#("72" 0 2 (font-lock-face paradox-star-face))) :commit "fae5d508ff519ba1fab21c51f46c0906fd82229f" :url "https://github.com/abo-abo/ace-link" :keywords '("convenience" "links" "avy"))
---
+++ elpa/projectile-20180227.1743/projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "projectile" "20180227.1743" "Manage and navigate projects in Emacs easily" '((emacs "24.3") (pkg-info "0.4")) :stars '(#("1953" 0 4 (font-lock-face paradox-star-face))) :commit "40b01b9d61ed310737d87e86b226bb4e1ed2e0cb" :url "https://github.com/bbatsov/projectile" :keywords '("project" "convenience"))
