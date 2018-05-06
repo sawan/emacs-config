0b3b5c6829ed356352a3342f74d40a27ce61b7ad
Sawan Vithlani <sawan@kili-soft.com>
2018-05-06 15:19:09 +1000

    Adjust hydra + try out origami parser; package updates

--- elpa/badwolf-theme-1.2/badwolf-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "badwolf-theme" "1.2" "Bad Wolf color theme" '((emacs "24")) :stars '(#("16" 0 2 (font-lock-face paradox-star-face))) :commit "24a557f92a702f632901a5b7bee59945a0a8cde9" :keywords '("themes") :url "https://github.com/bkruczyk/badwolf-emacs")
---
+++ elpa/badwolf-theme-20161004.15/badwolf-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; badwolf-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "badwolf-theme" "badwolf-theme.el" (23261 45413
+;;;;;;  0 0))
+;;; Generated autoloads from badwolf-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; badwolf-theme-autoloads.el ends here
