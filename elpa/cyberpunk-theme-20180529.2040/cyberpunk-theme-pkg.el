69290d6c307c090d4ca74cfd4da09df089e44df4
Sawan Vithlani <sawan@kili-soft.com>
2018-06-13 21:29:49 +1000

    Package update

--- elpa/cyberpunk-theme-20180529.2040/cyberpunk-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cyberpunk-theme" "20180529.2040" "Cyberpunk Color Theme" 'nil :stars '(#("128" 0 3 (font-lock-face paradox-star-face))) :commit "17f68f8ac70b712d2870ecb6adb3841b992074fa" :keywords '("color" "theme" "cyberpunk"))
---
+++ elpa/git-timemachine-20180607.120/git-timemachine-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-timemachine" "20180607.120" "Walk through git revisions of a file" '((emacs "24.3")) :stars '(#("430" 0 3 (font-lock-face paradox-star-face))) :commit "90a980578249c102da3e904fccdc9a2a5a0e7bcc" :keywords '("git") :url "https://github.com/pidu/git-timemachine")
